0376e366bfe0707d98851869ba327a7e00c370ba
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2020-11-19 11:12:19 -0800

    Remove unused variable.

--- trouble_client/src/Board.test.js
+++ trouble_client/src/Board.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Board from './Board';
 
 test('renders with DOM tree preserved', () => {
