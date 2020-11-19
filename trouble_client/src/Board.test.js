d8397c7517aa5158ae41a0f2e505904e0a1cb141
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2020-11-18 18:18:33 -0800

    Add unit tests for Board class. Add script for debugging React tests.

---
+++ trouble_client/src/Board.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import Board from './Board';
+
+test('renders with DOM tree preserved', () => {
+  render(<Board />);
+  expect(document.getElementById('SVGRoot').childElementCount).toBeGreaterThan(0);
+});
+
+
+test('all spaces present', () => {
+  render(<Board />);
+  for(var i = 0; i < 60; i++){
+    expect(document.getElementById('space' + i.toString())).toBeTruthy();
+  }
+});
+
+
+test('all spaces have an occupied, unoccupied, and suggestion ring', () => {
+  render(<Board />);
+  for(var i = 0; i < 60; i++){
+    const space = document.getElementById('space' + i.toString());
+    expect(space.getElementsByClassName('occupied_ring')).toBeTruthy();
+    expect(space.getElementsByClassName('unoccupied_ring')).toBeTruthy();
+    expect(space.getElementsByClassName('suggestion_ring')).toBeTruthy();
+  }
+});
