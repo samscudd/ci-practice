1551d6735ce3ab7ce68face0c9003d02a2f204d3
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2020-11-24 18:26:11 -0800

    Fix unit test with "conditional expect" statement.

--- trouble_client/src/game/Board.test.js
+++ trouble_client/src/game/Board.test.js
@@ -20,12 +20,11 @@ test('all spaces have an occupied or unoccupied ring (not both)', () => {
   for(var i = 0; i < 60; i++){
     const space = document.getElementById('space' + i.toString());
     const occupied_ring = space.querySelector('.occupied_ring');
-    const unoccupied_ring = space.querySelector('.unoccupied_ring');
+    const unoccupied_ring = space.querySelector('.unoccupied_ring'); 
+
+    const o_ring_presesnt = !!occupied_ring;
+    const u_ring_presesnt = !!unoccupied_ring;
     
-    if(occupied_ring != null && occupied_ring != undefined){
-      expect(unoccupied_ring).not.toBeTruthy()
-    } else {
-      expect(unoccupied_ring).toBeTruthy()
-    }
+    expect((o_ring_presesnt && !u_ring_presesnt) || (u_ring_presesnt && ! o_ring_presesnt)).toBeTruthy()
   }
 });
